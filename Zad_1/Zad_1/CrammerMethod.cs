0257aa524827a74cbfb229c401639c89f86c6419
Krzysztof Szczech <krzysztof.szczech@hotmail.com>
2015-10-05 22:58:36 +0200

    Init with loading matrices from file

---
+++ Zad_1/Zad_1/CrammerMethod.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Zad_1
+{
+    public class WrongMatrixSizeException : Exception
+    {
+        public WrongMatrixSizeException() : base("Sent matrix is not square") { }
+        public WrongMatrixSizeException(string message) : base(message) { }
+    }
+
+    public static class CrammerMethod
+    {
+        public static string Start(double[][] matrix)
+        {
+            return "DUPA";
+        }
+
+        public static double CountDeterminant(double[][] matrix)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                if (matrix[i].Length != matrix.Length)
+                {
+                    throw new WrongMatrixSizeException();
+                }
+            }
+
+            Stack<double[][]> matrices = new Stack<double[][]>();
+            matrices.Push(matrix);
+
+            while (matrices.Count > 0)
+            {
+                
+            }
+
+            return 0.0f;
+
+        }
+    }
+
+
+}
---
+++ Zad_1/Zad_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Zad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[][] loadedMatrix = FileLoader.LoadMatrixFromFile();
+            string result = CrammerMethod.Start(loadedMatrix);
+            Console.WriteLine(result);
+
+            Console.Write("Press enter to continue: ");
+            Console.ReadLine();
+        }
+    }
+}
